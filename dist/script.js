a61c6917a990715dd74806713bc2998092891508
Muhammad Ghazy Humaidi <muhammad.ghazy.hum@gmail.com>
2024-06-11 08:33:47 +0700

    adding dark mode to website

--- dist/script.js
+++ dist/script.js
@@ -2,10 +2,18 @@
 
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const toTopButton = document.querySelector("#toTop")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger-active");
     navMenu.classList.toggle("hidden");
+});
+// Tutup Hamburger di Luar Hamburger
+window.addEventListener("click", (e) => {
+    if(e.target != navMenu & e.target != hamburger){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
 });
 
 // Navbar fixed
@@ -16,7 +24,11 @@ window.onscroll = function() {
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add("navbar-fixed");
+        toTopButton.classList.add("flex");
+        toTopButton.classList.remove("hidden");
     }else {
+        toTopButton.classList.remove("flex");
+        toTopButton.classList.add("hidden");
         header.classList.remove("navbar-fixed");
     }
 }
@@ -24,3 +36,23 @@ window.onscroll = function() {
 const copyrightYears = document.querySelector("#tahun span");
 let getYear = new Date().getFullYear();
 copyrightYears.innerHTML = getYear.toString();
+
+// DARK MODE TOGGLE
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+    html.classList.toggle("dark");
+    if(darkToggle.checked){
+        localStorage.theme = "dark";
+    }else{
+        localStorage.theme = "light";
+    }
+});
+
+// pindahkan posisi toggle sesua mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+    } else {
+    darkToggle.checked = false
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // kalo valuenya media -> bakalan nyesuain sama sistem
   content: ['./index.html'], // untuk melacak perubahan file
   theme: {
     container : {
